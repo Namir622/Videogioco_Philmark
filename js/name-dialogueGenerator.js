5596cb83ab3888c7d0f486f1dbb18e0c6b3aea5c
Jumper222 <Ilsaltatore222@gmail.com>
2023-02-07 22:10:55 +0100

    riformattato il codice

--- js/name-dialogueGenerator.js
+++ js/name-dialogueGenerator.js
@@ -1,29 +1,68 @@
-function ragazzo01FNAMEGenerator() {
-    let nameGenBoy = Math.random() * (25 - 0);
-    let abc = Math.floor(nameGenBoy)
-    const GenFNAMEBoyArray = ["Benjamin", "James", "David", "Daniel", "Michael", "Christopher",
-        "John", "Andrew", "Matthew", "Joseph", "Nicholas", "William",
-        "Ryan", "Samuel", "Thomas", "Jonathan", "Eric", "Anthony",
-        "Matthew", "Samuel", "Justin", "Alexander", "Christopher", "Michael",
-        "William"];
-    let FName = GenFNAMEBoyArray[abc];
-    console.log(nameGenBoy);
-    return FName;
+function boy01FNameGenerator() {
+	const arrFNAMEBoy = [ //Grandezza: 25
+		"Benjamin",
+		"James",
+		"David",
+		"Daniel",
+		"Michael",
+		"Christopher",
+		"John",
+		"Andrew",
+		"Matthew",
+		"Joseph",
+		"Nicholas",
+		"William",
+		"Ryan",
+		"Samuel",
+		"Thomas",
+		"Jonathan",
+		"Eric",
+		"Anthony",
+		"Matthew",
+		"Samuel",
+		"Justin",
+		"Alexander",
+		"Christopher",
+		"Michael",
+		"William",
+	];
+	return arrFNAMEBoy[Randomizer(25)];
 }
-function ragazzo01LNAMEGenerator() {
-    let nameGenBoy = Math.random() * (25 - 0);
-    let abc = Math.floor(nameGenBoy)
-    const GenLNAMEBoyArray = [" Smith", " Brown", " Johnson", " Wilson", " Davis", " Lee",
-        " Martinez", " Robinson", " Wilson", " Gonzalez", " Parker", " Green",
-        " Edwards", " Turner", " Anderson", " Taylor", " Wilson", " Harris",
-        " Lewis", " Lee", " Hall", " Young", " Baker", " Hill",
-        " Campbell"];
-
-        let LName = GenLNAMEBoyArray[abc];
-    return LName;
-    
+function boy01LNameGenerator() {
+	const arrLNAMEBoy = [ //Grandezza: 25
+		" Smith",
+		" Brown",
+		" Johnson",
+		" Wilson",
+		" Davis",
+		" Lee",
+		" Martinez",
+		" Robinson",
+		" Wilson",
+		" Gonzalez",
+		" Parker",
+		" Green",
+		" Edwards",
+		" Turner",
+		" Anderson",
+		" Taylor",
+		" Wilson",
+		" Harris",
+		" Lewis",
+		" Lee",
+		" Hall",
+		" Young",
+		" Baker",
+		" Hill",
+		" Campbell",
+	];
+	return arrLNAMEBoy[Randomizer(25)];
 }
 function ragazzo01FULLNAMEGenerator() {
-    let FullName = ragazzo01FNAMEGenerator() + ragazzo01LNAMEGenerator();
-    return FullName;
-} //javascript fa schifo
+	let FullName = boy01FNameGenerator() + boy01LNameGenerator();
+	return FullName;
+}
+
+function Randomizer(x) {
+	return Math.floor(Math.random() * x);
+}
